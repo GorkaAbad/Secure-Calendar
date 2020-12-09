6eee1d095ba29cb487c3e1408d1ce04a77798e09
GorkaAbad <gorkaabad12@gmail.com>
2020-12-09 16:22:49 +0100

    Database helper finished

--- lib/database.dart
+++ lib/database.dart
@@ -1,45 +1,138 @@
 import 'dart:io';
 
-import 'package:flutter/cupertino.dart';
 import 'package:sqflite/sqflite.dart';
-import 'package:path/path.dart';
 import 'dart:async';
 import 'Task.dart';
+import 'package:path/path.dart';
 
-const String dbName = 'calendar';
+class DatabaseHelper {
+  static final _databaseName = 'databaseCalendar.db';
+  static final _databaseVersion = 1;
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
+  static final table = 'calendar';
 
-  databaseExists().then((value) => {
-        //If database exist then
-        //Reset for testing purposes
-        if (value)
-          {
-            //Delete
-            resetValues(),
-            //Create again
-            getDatabase(),
-            insertValues()
-          }
-        else
-          {
-            //Create
-            getDatabase(),
-            insertValues()
-          }
-      });
+  static final columnId = 'id';
+  static final columnName = 'name';
+  static final columnDescription = 'description';
+  static final columnDateStart = 'dateStart';
+  static final columnDateEnd = 'dateEnd';
+  static final columnColor = 'color';
+  static final columnDone = 'done';
 
-  getTask().then((value) => print(value));
+  DatabaseHelper._privateConstructor();
+
+  static final DatabaseHelper instance = DatabaseHelper._privateConstructor();
+
+  static Database _database;
+
+  Future<Database> get database async {
+    if (_database != null) return _database;
+    _database = await _initDatabase();
+    return _database;
+  }
+
+  _initDatabase() async {
+    String documentsDirectory = await getDatabasesPath();
+    String path = join(documentsDirectory, _databaseName);
+    return await openDatabase(path,
+        version: _databaseVersion, onCreate: _onCreate);
+  }
+
+  Future _onCreate(Database db, int version) async {
+    await db.execute('''
+    CREATE TABLE $table ( 
+      $columnId INTEGER PRIMARY KEY,$columnName TEXT,
+      $columnDescription TEXT,$columnDateStart INTEGER,
+      $columnDateStart INTEGER,$columnColor TEXT,
+      $columnDone INTEGER)
+    ''');
+  }
+
+  // Inserts a row in the database where each key in the Map is a column name
+  // and the value is the column value. The return value is the id of the
+  // inserted row.
+  Future<int> insert(Task task) async {
+    Database db = await instance.database;
+    return await db.insert(table, task.toMap());
+  }
+
+  // All of the rows are returned as a list of maps, where each map is
+  // a key-value list of columns.
+  Future<List<Task>> queryAllRows() async {
+    Database db = await instance.database;
+    List<Task> tasks = [];
+    List<Map<String, dynamic>> lis = await db.query(table);
+    lis.forEach((element) {
+      tasks.add(Task.fromMap(element));
+    });
+    return tasks;
+  }
+
+  // All of the methods (insert, query, update, delete) can also be done using
+  // raw SQL commands. This method uses a raw query to give the row count.
+  Future<int> queryRowCount() async {
+    Database db = await instance.database;
+    return Sqflite.firstIntValue(await db.rawQuery('SELECT COUNT(*) FROM $table'));
+  }
+
+  // We are assuming here that the id column in the map is set. The other
+  // column values will be used to update the row.
+  Future<int> update(Task task) async {
+    Database db = await instance.database;
+    Map<String, dynamic> row = task.toMap();
+    int id = row[columnId];
+    return await db.update(table, row, where: '$columnId = ?', whereArgs: [id]);
+  }
+
+  // Deletes the row specified by the id. The number of affected rows is
+  // returned. This should be 1 as long as the row exists.
+  Future<int> delete(Task task) async {
+    Database db = await instance.database;
+    int id = task.id;
+    return await db.delete(table, where: '$columnId = ?', whereArgs: [id]);
+  }
 }
 
+
+
+/*
+class DatabaseHelper {
+
+  DatabaseHelper();
+
+  void main() async {
+    WidgetsFlutterBinding.ensureInitialized();
+
+    databaseExists().then((value) =>
+    {
+      //If database exist then
+      //Reset for testing purposes
+      if (value)
+        {
+          //Delete
+          resetValues(),
+          //Create again
+          getDatabase(),
+          insertValues()
+        }
+      else
+        {
+          //Create
+          getDatabase(),
+          insertValues()
+        }
+    });
+
+    getTask().then((value) => print(value));
+  }
+
 //Future<List<Task>> task() async {
-Future<List<Map<String, dynamic>>> getTask() async {
-  final Database db = await getDatabase();
+  Future<List<Map<String, dynamic>>> getTask() async {
+    final Database db = await getDatabase();
 
-  final List<Map<String, dynamic>> maps = await db.query(dbName);
+    final List<Map<String, dynamic>> maps = await db.query(dbName);
 
-  return maps;
+    return maps;
 /*
     return List.generate(maps.length, (int i) => {
      Task(
@@ -52,43 +145,55 @@ Future<List<Map<String, dynamic>>> getTask() async {
       );
   });
  */
-}
+  }
 
-Future<Database> getDatabase() async {
-  return openDatabase(
-    join(await getDatabasesPath(), dbName + '.db'),
-    onCreate: (db, version) {
-      return db.execute("CREATE TABLE " +
-          dbName +
-          " (id INTEGER PRIMARY KEY,name TEXT,description TEXT,dateStart INTEGER,dateEnd INTEGER,color TEXT)");
-    },
-    version: 1,
-  );
-}
+  Future<Database> getDatabase() async {
+    return openDatabase(
+      join(await getDatabasesPath(), dbName + '.db'),
+      onCreate: (db, version) {
+        return db.execute("CREATE TABLE " +
+            dbName +
+            " (id INTEGER PRIMARY KEY,name TEXT,description TEXT,dateStart INTEGER,dateEnd INTEGER,color TEXT)");
+      },
+      version: 1,
+    );
+  }
 
-Future<void> insertTask(Task task) async {
-  final Database db = await getDatabase();
-  await db.insert(dbName, task.toMap(),
-      conflictAlgorithm: ConflictAlgorithm.replace);
-}
+  Future<void> insertTask(Task task) async {
+    final Database db = await getDatabase();
+    await db.insert(dbName, task.toMap(),
+        conflictAlgorithm: ConflictAlgorithm.replace);
+  }
 
-void insertValues() async {
-  await insertTask(Task(
-      DateTime.now().millisecondsSinceEpoch,
-      "Task1",
-      "Description of the task 1",
-      DateTime.now().millisecondsSinceEpoch,
-      DateTime.now().millisecondsSinceEpoch,
-      "Red",
-      true));
-}
+  void insertValues() async {
+    await insertTask(Task(
+        DateTime
+            .now()
+            .millisecondsSinceEpoch,
+        "Task1",
+        "Description of the task 1",
+        DateTime
+            .now()
+            .millisecondsSinceEpoch,
+        DateTime
+            .now()
+            .millisecondsSinceEpoch,
+        "Red",
+        true));
+  }
 
-void resetValues() {
-  getDatabasesPath()
-      .then((value) => {File(join(value, dbName + '.db')).delete(), getTask()});
-}
+  void resetValues() {
+    getDatabasesPath()
+        .then((value) =>
+    {
+      File(join(value, dbName + '.db')).delete(),
+      getTask()
+    });
+  }
 
-Future<bool> databaseExists() {
-  return getDatabasesPath()
-      .then((value) => File(join(value, dbName + '.db')).exists());
-}
+  Future<bool> databaseExists() {
+    return getDatabasesPath()
+        .then((value) => File(join(value, dbName + '.db')).exists());
+  }
+
+}*/
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,3 @@
-import 'package:calendar/main.dart';
-import 'package:flutter/animation.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/widgets.dart';
 
@@ -9,6 +7,7 @@ import 'package:table_calendar/table_calendar.dart';
 import 'package:local_auth/local_auth.dart';
 import 'Task.dart';
 import 'package:getwidget/getwidget.dart';
+import 'database.dart';
 
 List<Task> lis = [
   Task(3, "name1", "description", 3, 4, "color", true),
@@ -22,7 +21,6 @@ List<Task> lis = [
   Task(3, "name", "description", 3, 4, "color", true),
   Task(3, "name", "description", 3, 4, "color", true),
 ];
-double _paddingTop = 20;
 
 void main() {
   runApp(MyApp());
@@ -38,29 +36,17 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class Bottomsheep extends StatefulWidget {
-  @override
-  _Bottomsheep createState() => _Bottomsheep();
-}
-
-class _Bottomsheep extends State<Bottomsheep> {
-  @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
-  }
-}
-
 class MyHomePage extends StatefulWidget {
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
-//TODO: No funciona la lista, o bien por el expanded o el row, ninguna solucion todavia
 class _MyHomePageState extends State<MyHomePage> {
   CalendarController _controller;
   ScrollController _scrollController = ScrollController();
   ScrollController _scrollController2 = ScrollController();
+  final dbHelper = DatabaseHelper.instance;
+
   final color = 0xff30384c;
 
   List<Widget> listTask = [];
@@ -176,7 +162,7 @@ Safe Area
                 ),
                 calendarController: _controller),
             Padding(
-              padding: EdgeInsets.only(top: _paddingTop),
+              padding: EdgeInsets.only(top: 20),
             ),
             Expanded(
               child: ListView(
@@ -202,117 +188,3 @@ Safe Area
     );
   }
 }
-
-/*
- Scaffold(
-                bottomSheet: GFBottomSheet(
-                  enableExpandableContent: true,
-                  animationDuration: 300,
-                  controller: _controllerBottom,
-                  maxContentHeight: 270,
-                  stickyHeaderHeight: 70,
-                  stickyHeader: Container(
-                    decoration: BoxDecoration(
-                        color: Color(color),
-                        borderRadius: BorderRadius.only(
-                            topLeft: Radius.circular(50),
-                            topRight: Radius.circular(50)),
-                        boxShadow: [
-                          BoxShadow(color: Colors.black45, blurRadius: 0)
-                        ]),
-                    child: const GFListTile(
-                      title: Text(
-                        "Today",
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white,
-                          fontSize: 30,
-                        ),
-                      ),
-                    ),
-                  ),
-                  contentBody: Container(
-                    decoration: BoxDecoration(color: Color(color)),
-                    height: 100,
-                    child: ListView(
-                      padding: EdgeInsets.symmetric(horizontal: 15),
-                      shrinkWrap: true,
-                      physics: BouncingScrollPhysics(),
-                      children: [
-                        ListView.builder(
-                          shrinkWrap: true,
-                          physics: BouncingScrollPhysics(),
-                          controller: _scrollController,
-                          itemCount: listTask.length,
-                          itemBuilder: (context, index) {
-                            return listTask[index];
-                          },
-                        )
-                      ],
-                    ),
-                  ),
-                ),
-              ),
- */
-/*
- Expanded(
-              child: GestureDetector(
-                onVerticalDragDown: (details) {
-                  setState(() {
-                    print(containerKey.currentWidget.toString());
-                    //_paddingTop = MediaQuery.of(context).size.height *0.965;
-                  });
-                },
-                onPanUpdate: (tapInfo) {
-                  print('[*] Local: ' + tapInfo.localPosition.dy.toString());
-                  print('[*] Padding: ' + _paddingTop.toString());
-
-                  setState(() {
-                    if (tapInfo.localPosition.dy > 20 &&
-                        tapInfo.globalPosition.dy <
-                            MediaQuery.of(context).size.height * 0.965) {
-                      _paddingTop = tapInfo.localPosition.dy;
-                    }
-                  });
-                },
-                child: Container(
-                  padding: EdgeInsets.only(
-                    left: 30,
-                  ),
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.only(
-                          topLeft: Radius.circular(50),
-                          topRight: Radius.circular(50)),
-                      color: Color(color)),
-                  child: Stack(
-                    children: [
-                      Container(
-                        padding: EdgeInsets.only(top: 50),
-                        child: Text(
-                          "Today",
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            color: Colors.white,
-                            fontSize: 30,
-                          ),
-                        ),
-                      ),
-                      Container(
-                        height: MediaQuery.of(context).size.height,
-                        padding: EdgeInsets.only(top: 85),
-                        child: ListView.builder(
-                          shrinkWrap: true,
-                          physics: BouncingScrollPhysics(),
-                          controller: _scrollController,
-                          itemCount: listTask.length,
-                          itemBuilder: (context, index) {
-                            return listTask[index];
-                          },
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ),
- */
